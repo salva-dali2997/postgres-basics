8076f248017e03512b764701e56c6b102ef6f740
salva-dali2997 <64043261+salva-dali2997@users.noreply.github.com>
2024-04-01 12:55:28 -0400

    added a fetch command

---
+++ fetch_test_data.py
@@ -0,0 +1,19 @@
+import psycopg2
+from credentials import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+print(DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER)
+try:
+    conn = psycopg2.connect(database=DB_NAME,
+                            user=DB_USER,
+                            password=DB_PASS,
+                            host=DB_HOST,
+                            port=DB_PORT)
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM test")
+    # fetchmany(<index>) || cur.fetchall()
+    return_data = curs.fetchone()
+    print(return_data)
+    curs.close()
+    conn.close()
+    print("Database table fetched successfully")
+except:
+    print("Database table not fetched successfully")
